e188f1ae427ee2c4d344f4e1f5d5ecc76b209c89
Hajni83 <maro.hajnalka@gmail.com>
2021-10-28 15:54:56 +0200

    two task finished

---
+++ js/sum.js
@@ -0,0 +1,15 @@
+summation = (a, b) => {
+if (a + b === a + b){
+    return a + b;
+} else 
+    return 0;
+};
+
+    
+
+subtraction = (a, b) => {
+if ( a + b === a + b){
+    return a - b;
+}else
+    return 0;
+};
